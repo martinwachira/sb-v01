9c9058bafbb3ac945e1ff6861907311ce917b327
martinwachira <martinnwachira@gmail.com>
2022-06-13 18:27:34 +0300

    spring bootstrapped

---
+++ src/main/java/com/example/sbv01/SbV01Application.java
@@ -0,0 +1,13 @@
+package com.example.sbv01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbV01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbV01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sbv01/SbV01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbv01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbV01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
