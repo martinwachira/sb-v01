f28e50b404b600a280e6996b40b24801b9a9e01a
martinwachira <martinnwachira@gmail.com>
2022-06-17 04:18:10 +0300

    return json data > ctrl

--- src/main/java/io/javabrains/courseapi/topic/TopicController.java
+++ src/main/java/io/javabrains/courseapi/topic/TopicController.java
@@ -1,4 +1,21 @@
 package io.javabrains.courseapi.topic;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
 public class TopicController {
+    @RequestMapping("/topics")
+    public List<Topic> getAllTopics(){
+        return Arrays.asList(
+                new Topic("1", "Spring Boot", "SB Desc"),
+                new Topic("2", "Crypto Currency", "Crypto Desc"),
+                new Topic("3", "Artificial Intelligence", "AI Desc"),
+                new Topic("4", "Money Marketing", "MM Desc")
+        );
+
+    }
 }
